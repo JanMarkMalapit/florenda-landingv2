5514d06c93314baea40582d45f0ae191eb3bffc2
Jan Mark Malapit <99314044+JanMarkMalapit@users.noreply.github.com>
2022-12-07 10:42:53 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,105 @@
+// Nav open close
+const body = document.querySelector('body'),
+      navMenu = body.querySelector('.menu-content'),
+      navOpenBtn = body.querySelector('.navOpen-btn'),
+      navCloseBtn = navMenu.querySelector('.navClose-btn');
+
+if(navMenu && navOpenBtn){
+  navOpenBtn.addEventListener("click", () =>{
+    navMenu.classList.add("open");
+    body.style.overflowY = "hidden";
+  })
+}
+
+if(navMenu && navCloseBtn){
+  navCloseBtn.addEventListener("click", () =>{
+    navMenu.classList.remove("open");
+    body.style.overflowY = "scroll";
+  })
+}
+
+// Change header bg color
+window.addEventListener("scroll", () => {
+  const scrollY = window.pageYOffset;
+
+  if(scrollY > 5){
+    document.querySelector("header").classList.add("header-active");
+  }else{
+    document.querySelector("header").classList.remove("header-active");
+  }
+
+  // Scroll up button
+  const scrollUpBtn = document.querySelector('.scrollUp-btn');
+
+  if(scrollY > 250){
+    scrollUpBtn.classList.add("scrollUpBtn-active");
+  }else{
+    scrollUpBtn.classList.remove("scrollUpBtn-active");
+  }
+  
+  // Nav link indicator
+
+  const sections = document.querySelectorAll('section[id]');
+  sections.forEach(section =>{
+    const sectionHeight = section.offsetHeight,
+          sectionTop = section.offsetTop - 100;
+
+          let navId = document.querySelector(`.menu-content a[href='#${section.id}']`);
+          if(scrollY > sectionTop && scrollY <=  sectionTop + sectionHeight){
+            navId.classList.add("active-navlink");           
+          }else{
+            navId.classList.remove("active-navlink");     
+          }
+          
+          navId.addEventListener("click", () => {
+            navMenu.classList.remove("open");
+            body.style.overflowY = "scroll";
+          })
+  })
+})  
+  
+  
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const accordionContent = document.querySelectorAll(".accordion-content");
+
+accordionContent.forEach((item, index) => {
+    let header = item.querySelector("header");
+    header.addEventListener("click", () =>{
+        item.classList.toggle("open");
+
+        let description = item.querySelector(".description");
+        if(item.classList.contains("open")){
+            description.style.height = `${description.scrollHeight}px`; //scrollHeight property returns the height of an element including padding , but excluding borders, 
+            item.querySelector("i").classList.replace("fa-plus", "fa-minus");
+        }else{
+            description.style.height = "0px";
+            item.querySelector("i").classList.replace("fa-minus", "fa-plus");
+        }
+        removeOpen(index); //calling the funtion and also passing the index number of the clicked header
+    })
+})
+
+function removeOpen(index1){
+    accordionContent.forEach((item2, index2) => {
+        if(index1 != index2){
+            item2.classList.remove("open");
+
+            let des = item2.querySelector(".description");
+            des.style.height = "0px";
+            item2.querySelector("i").classList.replace("fa-minus", "fa-plus");
+        }
+    })
+}
